65338aec682a0b378159171ca5a40ba5f0874874
ardraFreely <ardra@freely.me>
2025-10-26 22:53:08 +1100

    added comments

--- tests/sauceDemoPurchase.spec.js
+++ tests/sauceDemoPurchase.spec.js
@@ -16,23 +16,30 @@ test.describe('SauceDemo E2E - Purchase flow', () => {
 
   const productName = 'Sauce Labs Fleece Jacket'
 
+
+  // Go to login page and login with valid credentials 
   await login.goto()
   await expect(login.loginBtn).toBeVisible()
   await login.login()
 
+  // verify that the products are loaded
   await expect(page).toHaveURL(/inventory/)
   const itemsCount = await products.items.count()
   expect(itemsCount).toBeGreaterThan(0)
 
+ // Add product to Cart
   await products.addProductToCart(productName)
   await expect(products.cartItemsCount).toHaveText('1')
  
+  // Open cart and verify product is present
   await products.openCart()
   await cart.verifyItemInCart(productName)
 
+  // Enter shipping details and checkout
   await cart.checkout()
   await checkout.enterShippingDetails()
 
+ // Finish checkout and confirm purchase success message
   await checkout.finishOrder()
   await checkout.verifyPurchaseSuccess()
  })
